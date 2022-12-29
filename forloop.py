d210f442ed4b15f557ceea29cbaf2bb3807ecbc3
VaishnaviRaj20 <121537484+VaishnaviRaj20@users.noreply.github.com>
2022-12-29 20:30:16 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,31 @@
+# for i in[0,1,2]:
+#     print("ram")
+# for i in range(3):
+#     print("ram")
+
+# for _ in range(4):
+#     print("ram") # there is no imapact of i
+
+# print("meow\n"*3,end="") # for printing code in next line
+#print("meow\n","xyz")
+# while True:
+#     n=int(input("what is your n"))
+#     if n>0:
+#         continue
+#     else:
+#         break
+
+# while True:
+#     n=int(input("what is your n"))
+#     if n>0:
+#         break
+# for _ in range(n):
+#     print("ram")
+
+# students=["akash","abhishek","adil"]
+# for i in students:
+#     print(i)
+
+students=["om","anwee","abhi"]
+for i in range(len(students)):
+    print(i+1,students[i])
